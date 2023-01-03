f74512eea1e4fb65ed390cd8e30c9eb4110c47d2
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-01-03 03:41:30 +0100

    crazy progress on pinmappingscreen

--- src/main/java/me/jonasjones/microcraft/gui/screens/board/AdvancedPinMappingScreen.java
+++ src/main/java/me/jonasjones/microcraft/gui/screens/board/AdvancedPinMappingScreen.java
@@ -13,6 +13,9 @@ public class AdvancedPinMappingScreen extends Screen {
         this.parent = parent;
     }
 
+    protected void init() {
+    }
+
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
         drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 16777215);
